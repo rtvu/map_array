721bf2be23a7ad9b4ef747b6164141c2b2292099
ryan <rtvu87@gmail.com>
2021-06-13 03:03:02 +0000

    Added ex_doc and it's configuration

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule MapArray.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "MapArray",
+      source_url: "https://github.com/rtvu/map_array",
+      docs: [
+        main: "MapArray"
+      ]
     ]
   end
 
@@ -19,6 +24,7 @@ defmodule MapArray.MixProject do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
